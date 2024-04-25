30e8e52f7473c962eaef941d04cb4831f07012c3
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-04-24 18:09:24 -0700

    Add file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,133 +1,19 @@
 #!/usr/bin/python3
+"""Modules for class base
+It takes a private instance object
 """
-This module implements `base` class of all other classes in this project.
-The goal of it is to manage id attribute in all your future classes
-and to avoid duplicating the same code (by extension, same bugs)
-"""
-import json
-import turtle
 
 
 class Base:
+    """Declaring the base class
     """
-    implementation
-    """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """initialization
-        Args:
-            id (int, optional): object id. Defaults to None.
+        """Defining the class constructor
         """
-        if id is not None:
+        if id != None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries: dict):
-        """list to json"""
-        if list_dictionaries is None:
-            return "[]"
-        else:
-            return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """save to json file"""
-        filename = cls.__name__ + ".json"
-        text = []
-        if list_objs is not None:
-            for lst in list_objs:
-                text.append(lst.to_dictionary())
-        with open(filename, mode="w", encoding="utf-8") as f:
-            return f.write(Base.to_json_string(text))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """transform a JSON string representation `json_string` to a list"""
-        if json_string is None or len(json_string) == 0:
-            return []
-        else:
-            return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """create a new object from dictionary"""
-        if cls.__name__ == "Rectangle":
-            new = cls(10, 10)
-        elif cls.__name__ == "Square":
-            new = cls(10, 10)
-        new.update(**dictionary)
-        return new
-
-    @classmethod
-    def load_from_file(cls):
-        """load from file"""
-        filename = cls.__name__ + ".json"
-        object_created = []
-        with open(filename, 'r') as f:
-            file_string = f.read().replace('\n', '')
-            data = cls.from_json_string(file_string)
-            for el in data:
-                object_created.append(cls.create(**el))
-
-        return object_created
-
-    @classmethod
-    def save_to_file_csv(cls, list_objs):
-        """save to csv file"""
-        filename = cls.__name__ + ".csv"
-        content = ""
-        text = []
-        if list_objs is not None:
-            content += ','.join(list_objs[0].to_dictionary().keys())
-            content += '\n'
-            for lst in list_objs:
-                content += ','.join(
-                    map(str, lst.to_dictionary().values())
-                )
-                content += '\n'
-
-        with open(filename, mode="w", encoding="utf-8") as f:
-            return f.write(content)
-
-    @classmethod
-OBOB    def load_from_file_csv(cls):
-OB        """load from csv"""
-        filename = cls.__name__ + ".csv"
-OB        object_created = []
-
-OB        with open(filename, 'r') as f:
-OB            header = f.readline().replace('\n', '').split(',')
-OB            for el in f.readlines():
-OB                values = map(int, el.replace('\n', '').split(','))
-OB                data = dict(zip(header, values))
-OB                object_created.append(cls.create(**data))
-
-        return object_created
-OB
-    @classmethod
-    def draw(cls, list_rectangles, list_squares):
-        """draw the figure
-        """
-OB        window = turtle.Screen()
-        pen = turtle.Pen()
-        figures = list_rectangles + list_squares
-
-        for fig in figures:
-            pen.up()
-OB            pen.goto(fig.x, fig.y)
-            pen.down()
-            pen.forward(fig.width)
-            pen.right(90)
-            pen.forward(fig.height)
-            pen.right(90)
-            pen.forward(fig.width)
-            pen.right(90)
-            pen.forward(fig.height)
-            pen.right(90)
-
-        window.exitonclick()
